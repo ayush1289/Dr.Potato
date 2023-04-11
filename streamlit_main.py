202a3681278d8a8b92b261f920d7b64d29b9ff23
ayush1289 <ayushshankarpurkar11@gmail.com>
2023-04-11 20:19:06 +0530

    added a streamlit file

---
+++ streamlit_main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+MODEL =tf.keras.models.load_model("model/2")
+
+CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
+
+
+
+st.title('DR. POTATO')
+
+
+image = st.file_uploader("please upload image here!", type =["png","jpeg","jpg"])
+if image is not None:
+   image = image.read()
+   img = Image.open(BytesIO(image))
+   st.image(img, caption='Uploaded Image')
+   
+   img_batch = np.expand_dims(img, 0)
+   prediction = MODEL.predict(img_batch)
+   predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+   confidence = np.max(prediction[0])
+
+   st.text(predicted_class)
+   st.text(confidence)
+
